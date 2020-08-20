0b97ff23a5b43e4c5cfb5120da1dae2db6ba4a74
Vignesh Sivarajan <30792536+hacksparrow98@users.noreply.github.com>
2020-08-20 17:44:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -111,5 +111,5 @@ app.delete("/events/:id", function(req, res){
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Server Has Started!");
-	console.log("Server webhook");
+	console.log("Github webhook");
 });
